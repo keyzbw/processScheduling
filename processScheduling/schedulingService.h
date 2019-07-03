ab1f34ab82a6a1abb04dd5449c2c94172977fd0d
干子雄 <g64848066@gmail.com>
2019-07-03 16:27:48 +0800

    FIFO大致流程

--- processScheduling/schedulingService.h
+++ processScheduling/schedulingService.h
@@ -5,7 +5,7 @@ class schedulingService
 public:
 	schedulingService();
 	void PCBinit(int n,PCB* pend, PCB* ready, PCB* finish, PCB* ioa, PCB* iob);
-	void FIFO(PCB* pend, PCB* ready, PCB* finish, PCB* ioa, PCB* iob);
+	void FIFO(int time,PCB* pend, PCB* ready, PCB* finish, PCB* ioa, PCB* iob);
 	~schedulingService();
 };
 
